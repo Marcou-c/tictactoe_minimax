3e410b256d68293e86a763b4bc8e721a175d215f
Marco Ugalde <marco.u.c0312@gmail.com>
2025-10-18 12:53:28 -0600

    changes in files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-xs sm:text-sm text-gray-400 text-center w-full mt-8">
-      © {new Date().getFullYear()} Marco Ugalde. Todos los derechos reservados.
+      © {new Date().getFullYear()} ig: marcou_c. Todos los derechos reservados.
     </footer>
   );
 }
